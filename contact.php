c0397ef63d1b9f641b768b00c27c215d316d836b
Andrew Springman <awspringman@gmail.com>
2018-06-21 16:40:51 -0400

    integrate dynamic data with design

---
+++ contact.php
@@ -0,0 +1,16 @@
+<?php
+$name = $_REQUEST['Name'];
+$email = $_REQUEST['Email'];
+$message = $_REQUEST['Message'];
+
+$msg = "Someone filled out the contact form on andrewspringmanart.com
+
+NAME:  $name
+EMAIL: $email
+MESSAGE:
+
+$message
+";
+
+mail("andrew@andrewspringmanart.com","Website Contact from $name",$msg);
+?>
